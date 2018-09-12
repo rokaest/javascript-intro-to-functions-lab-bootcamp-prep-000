caaaf070529030da70bdc55c96baf6858b1b05f5
Ro <rvnadreau@ucdavis.edu>
2018-09-12 23:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
-    return "YES, INDEED!"
+    return "YES INDEED!"
   }
   else {
-  return "I can't hear you!"
+  return "I can\'t hear you!"
 }
 } 
 
